f6ec6c66cf5c9cdce932328f2002f56b0cd0ed2f
HubyLiang <hubyliang@foxmail.com>
2018-03-08 17:48:48 +0800

    Java多线程并发

---
+++ src/main/java/com/liang/thread/ThreadDemo2.java
@@ -0,0 +1,66 @@
+package com.liang.thread;
+
+/**
+ * 两个线程交替操作一个变量,该变量的初始值为0,要求一个加1, 一个减1, 共10轮.
+ * <p>
+ * 生产者/消费者模式
+ * <p>
+ * 1. 线程  操作  资源类
+ * 2. 高内聚 + 低耦合
+ */
+public class ThreadDemo2 {
+
+    public static void main(String[] args) {
+
+        final ShareDemo2 shareDemo = new ShareDemo2();
+
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0;i<10;i++){
+                    try {
+                        shareDemo.increment();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        },"Increment").start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i=0;i<10;i++){
+                    try {
+                        shareDemo.decrement();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        },"Decrement").start();
+    }
+
+}
+
+
+class ShareDemo2 {
+    private int number = 0;
+
+    public synchronized void increment() throws InterruptedException {
+        if (number != 0) {
+            this.wait();
+        }
+        number++;
+        System.out.println(Thread.currentThread().getName() + "\t" + number);
+        this.notifyAll();
+    }
+
+    public synchronized void decrement() throws InterruptedException {
+        if (number == 0) {
+            this.wait();
+        }
+        number--;
+        System.out.println(Thread.currentThread().getName() + "\t" + number);
+        this.notifyAll();
+    }
+
+}
